5c91c9c851705273be3f18dd4ed02ef349e2226f
Priyanka <priyanka980611@gmail.com>
2020-08-05 23:14:23 -0700

    Updates

---
+++ precache-manifest.beb48351aaa3ca5db6120acfa7894f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388f273b723d70dc0e4c835ce158eef3",
+    "url": "/meme/index.html"
+  },
+  {
+    "revision": "087b696fc4a5c2030665",
+    "url": "/meme/static/css/main.b5ad4afb.chunk.css"
+  },
+  {
+    "revision": "7ef087e62239f565378e",
+    "url": "/meme/static/js/2.0cab3e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme/static/js/2.0cab3e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087b696fc4a5c2030665",
+    "url": "/meme/static/js/main.300968b1.chunk.js"
+  },
+  {
+    "revision": "60ea7f8d63a6be53f64b",
+    "url": "/meme/static/js/runtime-main.15acd509.js"
+  }
+]);
